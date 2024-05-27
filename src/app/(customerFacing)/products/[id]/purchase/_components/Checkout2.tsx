170d4640e458d8284b3a01729314c60538ba4df7
babish9887 <babish9887@gmail.com>
2024-05-27 21:18:19 +0545

    minor change

--- src/app/(customerFacing)/products/[id]/purchase/_components/Checkout2.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/Checkout2.tsx
@@ -112,6 +112,12 @@ function Checkout2({product}:any) {
             </CardFooter>
       </Card>
       </form>
+      <div>
+            <h2 className='font-semibold text-2xl'>Use Following credentials for esewa</h2>
+            <p>esewaID: 9806800001/2/3/4/5</p>
+            <p>Password: 1122 or Nepal@123</p>
+            <p>Token/Code: 123456</p>
+      </div>
       </div>  
       )
 }
