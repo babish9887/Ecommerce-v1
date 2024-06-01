ba34fdef69533a15e499d7c21420bece473d3c2b
babish9887 <babish9887@gmail.com>
2024-06-01 06:57:54 -0700

    Organized Code

--- src/app/actions/getSession.ts
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from "next-auth";
-import MyAuthOptions from "../../lib/authOptions";
-
-export default async function getSession(){
-      return await  getServerSession(MyAuthOptions);
-}
