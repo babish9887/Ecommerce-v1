fd52129ffce1204c31a0185591bcc0ce7be780f7
babish9887 <babish9887@gmail.com>
2024-06-01 07:14:16 -0700

    Organized  code

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPOoHUcV7Uve0Mbg7xZBzOqiaw9KMExLA",
-  authDomain: "digital-oasis-35451.firebaseapp.com",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: "digital-oasis-35451",
   storageBucket: "digital-oasis-35451.appspot.com",
-  messagingSenderId: "263797926684",
+  messagingSenderId: process.env.SENDER_ID,
   appId: "1:263797926684:web:aeaa3b5aa9bfdc264768d9",
   measurementId: "G-0161DQQTBM"
 };
